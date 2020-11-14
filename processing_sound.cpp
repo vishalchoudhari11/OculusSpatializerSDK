b404c6e6487b32b2b27fd921715c38fe85dc1262
Vishal Choudhari <choudharivishal9@gmail.com>
2020-11-15 00:21:02 +0530

    v6

--- processing_sound.cpp
+++ processing_sound.cpp
@@ -5,22 +5,12 @@ using namespace std;
 
 int main(){
 	cout << "My neighbours are noisy!" << endl;
-
-    int major, minor, patch;
-    const char *VERSION_STRING;
-
-    VERSION_STRING = ovrAudio_GetVersion( &major, &minor, &patch );
-    printf( "Using OVRAudio: %s\n", VERSION_STRING );
-    
-    
-    
     
     ovrAudioContext context;
+    
     ovrAudioEnable room_setting_SRM = ovrAudioEnable_SimpleRoomModeling;
     ovrAudioEnable room_setting_LR  = ovrAudioEnable_LateReverberation;
     ovrAudioEnable room_setting_RR  = ovrAudioEnable_RandomizeReverb;
-    
-    
     
     ovrAudioContextConfiguration config = {};
     
@@ -28,11 +18,51 @@ int main(){
     config.acc_SampleRate = 48000;
     config.acc_BufferLength = 512;
     config.acc_MaxNumSources = 16;
-    
-    ovrAudio_DestroyContext( AudioContext );
-    ovrAudio_Shutdown();
 
+    shutdownOvrAudio(context);
+    
     cout << "My neighbours are noisy again!" << endl;
    	return 0;
 
+}
+
+
+void setup()
+{
+    // Version checking is not strictly necessary but it's a good idea!
+    int major, minor, patch;
+    const char *VERSION_STRING;
+
+    VERSION_STRING = ovrAudio_GetVersion( &major, &minor, &patch );
+    printf( "Using OVRAudio: %s\n", VERSION_STRING );
+
+    if ( major != OVR_AUDIO_MAJOR_VERSION ||
+         minor != OVR_AUDIO_MINOR_VERSION )
+    {
+      printf( "Mismatched Audio SDK version!\n" );
+    }
+
+    ovrAudioContextConfiguration config = {};
+
+    config.acc_Size = sizeof( config );
+    config.acc_SampleRate = 48000;
+    config.acc_BufferLength = 512;
+    config.acc_MaxNumSources = 16;
+
+    ovrAudioContext context;
+
+    if ( ovrAudio_CreateContext( &context, &config ) != ovrSuccess )
+    {
+      printf( "WARNING: Could not create context!\n" );
+      return;
+    }
+    
+    shutdownOvrAudio(context);
+    
+}
+
+void shutdownOvrAudio(ovrAudioContext c)
+{
+   ovrAudio_DestroyContext(c);
+   ovrAudio_Shutdown();
 }
