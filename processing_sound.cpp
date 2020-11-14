4a892559733ffdac42c70e4a1c2265c7256db47f
Vishal Choudhari <choudharivishal9@gmail.com>
2020-11-14 17:24:58 +0530

    v3

--- processing_sound.cpp
+++ processing_sound.cpp
@@ -2,18 +2,11 @@
 #include "OVR_Audio.h"
 using namespace std;
 
-void shutdownOvrAudio(ovrAudioContext Context)
-{
-   ovrAudio_DestroyContext(Context);
-   ovrAudio_Shutdown();
-}
-
 
 int main(){
 	cout << "My neighbours are noisy!" << endl;
 
 	ovrAudioContext context;
-	shutdownOvrAudio(context);
 
    	return 0;
 
