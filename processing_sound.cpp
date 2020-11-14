f657dc831889a47b532c91ceb9a5be7ac2832bf1
Vishal Choudhari <choudharivishal9@gmail.com>
2020-11-14 23:49:52 +0530

    v4

--- processing_sound.cpp
+++ processing_sound.cpp
@@ -6,8 +6,32 @@ using namespace std;
 int main(){
 	cout << "My neighbours are noisy!" << endl;
 
-	ovrAudioContext context;
+    int major, minor, patch;
+    const char *VERSION_STRING;
 
+    VERSION_STRING = ovrAudio_GetVersion( &major, &minor, &patch );
+    printf( "Using OVRAudio: %s\n", VERSION_STRING );
+    
+    
+    
+    
+    ovrAudioContext context;
+    ovrAudioEnable room_setting_SRM = ovrAudioEnable_SimpleRoomModeling;
+    ovrAudioEnable room_setting_LR  = ovrAudioEnable_LateReverberation;
+    ovrAudioEnable room_setting_RR  = ovrAudioEnable_RandomizeReverb;
+    
+    
+    
+    ovrAudioContextConfiguration config = {};
+    
+    config.acc_Size = sizeof( config );
+    config.acc_SampleRate = 48000;
+    config.acc_BufferLength = 512;
+    config.acc_MaxNumSources = 16;
+    
+    
+
+    cout << "My neighbours are noisy again!" << endl;
    	return 0;
 
 }
